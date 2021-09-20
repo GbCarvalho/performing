a9cb452953045b1235598f7c20f4c7843ebd72ce
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-09-20 11:38:22 -0300

    Initialized add to wishlist component

---
+++ components/AddProductToWishlist.tsx
@@ -0,0 +1,17 @@
+interface AddProductToWishlistProps {
+  onAddProductToWishlist: () => void;
+  onRequestClose: () => void;
+}
+
+export function AddProductToWishlist({
+  onAddProductToWishlist,
+  onRequestClose,
+}: AddProductToWishlistProps) {
+  return (
+    <span>
+      Deseja mesmo adicionar esse produto aos favoritos?
+      <button onClick={onAddProductToWishlist}>Sim</button>
+      <button onClick={onRequestClose}>Não</button>
+    </span>
+  );
+}
